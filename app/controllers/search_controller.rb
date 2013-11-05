d6153b1a8dbdb92138fd1b6bfae9be690490385b
Lukas Weinwurm <lweinwurm@hotmail.com>
2013-11-05 19:08:41 +0100

    search index page show only not following users

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,12 @@
 class SearchController < ApplicationController
+	before_filter :authenticate_user!
+
 	def search
-		@users = User.all_except(current_user)
+		@followingusers = current_user.following_relationships.map(&:leader)
+		@users = User.all_except(current_user).where("id NOT IN (?)", @followingusers.map(&:id))
 	end
 
 	def result
-		@results = User.search_by_username(params[:search_term])
+		@results = User.search_by_username(params[:search_term]).all_except(current_user)
 	end
 end
